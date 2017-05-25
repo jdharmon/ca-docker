bac765e517a8b3dde3e464c2f3272ed27f94b9ce
Jason Harmon <jason@jdharmon.com>
2017-05-24 22:45:53 -0400

    Changed extensions to be Windows friendly.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,11 @@
 
 new_ca () {
 	echo "*** Creating CA ***"
+	echo
 	mkdir certs crl newcerts private
 	touch index.txt
 	echo 1000 > serial
-	openssl req -new -x509 -extensions v3_ca -out cacert.pem -keyout private/cakey.pem 
+	openssl req -new -x509 -extensions v3_ca -out cacert.crt -keyout private/cakey.key
 }
 
 #Check for existing CA
