e8e14b66e3f79c7e22a45df9d09c25ee741de240
Jason Harmon <jdharmon@sentara.com>
2017-06-07 15:58:33 +0000

    * Added server_cert to config. * Added create command. Closes #1.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+set -e
+
+usage() {
+	echo "Usage: docker run -it -v /path/to/ca:/ca jdharmon/ca create server|usr <certname>"
+}
+
 new_ca () {
 	echo "*** Creating CA ***"
 	echo
@@ -9,10 +15,33 @@ new_ca () {
 	openssl req -new -x509 -extensions v3_ca -out cacert.crt -keyout private/cakey.key
 }
 
+create_cert() {
+	if [ "$1" != "server" ] && [ "$1" != "usr" ]; then
+		usage
+		exit 2
+	fi
+	
+	extensions="$1_cert"
+	certname=$2
+
+	echo "*** Creating new $1 certificate ***"
+	openssl req -new -sha256 -out $certname.csr -keyout private/$certname.key
+	openssl ca -extensions $extensions -in $certname.csr -out certs/$certname.crt
+	echo
+	echo "Certificate: certs/$certname.crt"
+	echo "Private key: private/$certname.key"
+	rm $certname.csr
+}
+
 #Check for existing CA
 if [ ! -f index.txt ]; then
     new_ca
 fi
 
-#Run CMD
-$1
+case "$1" in
+	create)
+		create_cert $2 $3
+		;;
+	*)
+		usage
+esac
